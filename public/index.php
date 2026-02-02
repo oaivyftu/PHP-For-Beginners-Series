9ddcd687438f8d751a476eb938add130c27f9a39
vincent <oaivyftu@gmail.com>
2026-02-02 18:52:22 -0500

    update

--- public/index.php
+++ public/index.php
@@ -4,17 +4,12 @@ use Core\Router;
 use Core\Session;
 use Core\ValidationException;
 
-session_start();
-
 const BASE_PATH = __DIR__.'/../';
 
+require BASE_PATH."vendor/autoload.php";
 require BASE_PATH.'Core/functions.php';
 
-spl_autoload_register(function ($class) {
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-
-    require base_path("{$class}.php");
-});
+session_start();
 
 require base_path('bootstrap.php');
 

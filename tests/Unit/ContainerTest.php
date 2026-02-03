0ce166c048ecb17e29ad33dc4f522c7307d00a11
vincent <oaivyftu@gmail.com>
2026-02-03 12:01:15 -0500

    add test

---
+++ tests/Unit/ContainerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Core\Container;
+
+test('it can resolve something out of the container', function () {
+    // arrange
+    $container = new Container();
+
+    $container->bin
+    // act
+    // assert/expect
+})

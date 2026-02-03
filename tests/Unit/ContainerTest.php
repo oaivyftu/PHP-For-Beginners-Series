e0f96abce6cedf63a46a2ce54846f676e5b19853
vincent <oaivyftu@gmail.com>
2026-02-03 12:41:55 -0500

    update test

--- tests/Unit/ContainerTest.php
+++ tests/Unit/ContainerTest.php
@@ -6,7 +6,11 @@ test('it can resolve something out of the container', function () {
     // arrange
     $container = new Container();
 
-    $container->bin
+    $container->bind('foo', fn() => 'bar');
+
     // act
+    $result = $container->resolve('foo');
+
     // assert/expect
-})
+    expect($result)->toEqual('bar');
+});
